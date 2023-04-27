647c377fa0b6c02d74ec3580a0df79d538dd8862
Hanah R <hanahratican@gmail.com>
2023-04-27 16:46:42 -0700

    Added code to userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,106 @@
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require("../models");
 
+const userControllers = {
+
+    async getUsers(req, res) {
+        try {
+            const dbUserData = await User.find({}).populate('thoughts').populate('friends');
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async getSingleUser(req, res) {
+        try {
+            const dbUserData = await User.findOne({ _id: req.params.userId }).populate('thoughts').populate('friends');
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const dbUserData = await User.create(req.body);
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const dbUserData = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                req.body,
+                { new: true }
+            );
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const dbUserData = await User.findOneAndDelete({ _id: req.params.userId });
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            } else {
+                await Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
+                res.json(dbUserData);
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async addFriend(req, res) {
+        try {
+            const dbUserData = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { new: true }
+            );
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async removeFriend(req, res) {
+        try {
+            const dbUserData = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { new: true }
+            );
+            if (!dbUserData) {
+                return res.status(404).json({ message: 'No user found with this id!' });
+            }
+            res.json(dbUserData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+};
+
+module.exports = userControllers;
