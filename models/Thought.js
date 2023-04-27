2303a4e09974a76724009b3e2f9eec07c600c817
Hanah R <hanahratican@gmail.com>
2023-04-26 18:03:24 -0700

    Added date.js to utils folder, finished setting up the models

---
+++ models/Thought.js
@@ -0,0 +1,66 @@
+const { Schema, model, Types  } = require("mongoose");
+const dateFormat = require('../utils/date');
+
+const reactionsSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId(),
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+    }, 
+)
+
+
+const thoughtsSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionsSchema],
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    },
+)
+
+thoughtsSchema.virtual("reactionCount").get(function () {
+    return this.reactions.lenth;
+});
+
+const Thought = model("Thought", thoughtsSchema);
+
+module.exports = Thought;
